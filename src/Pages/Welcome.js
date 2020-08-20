bc608b9abbfa1d3b7dedcf60bb58e9c95ff8e548
= <leoncai197@gmail.com>
2020-08-20 18:47:46 -0500

    Created Welcome Page and Icon

---
+++ src/Pages/Welcome.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+import AppLogo from '../Icons/app-logo.svg'
+import StravaButton from '../Images/strava-button.png'
+import '../Styles/Welcome.css'
+
+class Welcome extends Component {
+
+    render() {
+        return (
+            <div>
+                <img class="img-center" src={AppLogo}></img>
+                <h1>Run Trckr</h1>
+                <h5>Compete against your friends in Strava challenges</h5>
+                <div class="button-center">
+                    <img src={StravaButton}></img>
+                </div>
+                <h6 class="link-text">By signing in, you agree to our Privacy Policy</h6>
+            </div>
+        )
+    }
+}
+
+export default Welcome;
