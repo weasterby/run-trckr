dc3bdea97f0175a5478fd15d2abb693d3a405ab6
Will Easterby <william.easterby@gmail.com>
2020-08-25 15:42:51 -0500

    Add endpoints

--- api/database.js
+++ api/database.js
@@ -27,9 +27,35 @@ module.exports.createUser = async function(profile){
 
     return {
         id: results.rows[0].id,
-        name: results.rows[0].completed ? results.rows[0].name : null
+        name: results.rows[0].completed ? results.rows[0].name : null,
+        active_group: null,
+        active_contest: null,
+        active_role: null
     }
 
+};
+
+module.exports.getRole = async function(user_id, group_id, contest_id) {
+    const client = await pool.connect();
+    await client.query("BEGIN;");
+    let results;
+    try {
+        results = await client.query("SELECT role FROM user_contests WHERE \"user\" = $1 AND \"group\" = $2 AND contest = $3",
+            [user_id, group_id, contest_id]);
+        await client.query("COMMIT;");
+    }
+    catch (e) {
+        console.error(e);
+        await client.query("ROLLBACK;");
+        throw(e);
+    }
+    finally {
+        client.release();
+    }
+    if (results !== undefined && results.rows.length > 0)
+        return results.rows[0].role;
+    else
+        return null;
 };
 
 module.exports.getUser = async function(id) {
@@ -109,4 +135,115 @@ module.exports.updateUser = async function(id, data) {
 
     return true;
 
+};
+
+module.exports.getGroupActivities = async function(group_id, contest_id) {
+    const client = await pool.connect();
+    await client.query("BEGIN;");
+    let results;
+    try {
+        results = await client.query("SELECT activity.name, activity.start_date_local, activity.distance," +
+            "activity.moving_time, activity.average_speed, activity.total_elevation_gain, activity.type, \"user\".name AS athlete FROM\n" +
+            "contest_activities AS contest\n" +
+            "LEFT JOIN activities AS activity\n" +
+            "ON contest.activity = activity.id\n" +
+            "LEFT JOIN users AS \"user\"\n" +
+            "ON activity.\"user\" = \"user\".id\n" +
+            "WHERE contest.contest = $1 AND contest.\"group\" = $2 AND activity.complete = true\n" +
+            "ORDER BY start_date_local DESC;", [contest_id, group_id]);
+        await client.query("COMMIT;");
+    }
+    catch (e) {
+        console.error(e);
+        await client.query("ROLLBACK;");
+        throw(e);
+    }
+    finally {
+        client.release();
+    }
+    if (results !== undefined)
+        return results.rows;
+    else
+        return {};
+};
+
+module.exports.getMyGroupActivities = async function(group_id, contest_id, user_id) {
+    const client = await pool.connect();
+    await client.query("BEGIN;");
+    let results;
+    try {
+        results = await client.query("SELECT activity.name, activity.start_date_local, activity.distance," +
+            "activity.moving_time, activity.average_speed, activity.total_elevation_gain, activity.type, \"user\".name AS athlete FROM\n" +
+            "contest_activities AS contest\n" +
+            "LEFT JOIN activities AS activity\n" +
+            "ON contest.activity = activity.id\n" +
+            "LEFT JOIN users AS \"user\"\n" +
+            "ON activity.\"user\" = \"user\".id\n" +
+            "WHERE contest.contest = $1 AND contest.\"group\" = $2 AND contest.\"user\" = $3 AND activity.complete = true\n" +
+            "ORDER BY start_date_local DESC;", [contest_id, group_id, user_id]);
+        await client.query("COMMIT;");
+    }
+    catch (e) {
+        console.error(e);
+        await client.query("ROLLBACK;");
+        throw(e);
+    }
+    finally {
+        client.release();
+    }
+    if (results !== undefined)
+        return results.rows;
+    else
+        return {};
+};
+
+module.exports.getGroupLeaderBoard = async function(group_id, contest_id) {
+    const client = await pool.connect();
+    await client.query("BEGIN;");
+    let results;
+    try {
+        results = await client.query("SELECT \"user\".name, SUM(challenge.points) AS total_points FROM\n" +
+            "user_challenges AS challenge\n" +
+            "LEFT JOIN users AS \"user\" ON challenge.\"user\" = \"user\".id\n" +
+            "WHERE challenge.\"group\" = $1 AND challenge.contest = $2\n" +
+            "GROUP BY challenge.\"user\", \"user\".name\n" +
+            "ORDER BY total_points DESC;", [group_id, contest_id]);
+        await client.query("COMMIT;");
+    }
+    catch (e) {
+        console.error(e);
+        await client.query("ROLLBACK;");
+        throw(e);
+    }
+    finally {
+        client.release();
+    }
+    if (results !== undefined)
+        return results.rows;
+    else
+        return {};
+};
+
+module.exports.getCurrentContests = async function(group_id, contest_id, timestamp) {
+    const client = await pool.connect();
+    await client.query("BEGIN;");
+    let results;
+    try {
+        results = await client.query("SELECT name, description, start_date, end_date, awards\n" +
+            "FROM challenges WHERE \"group\" = $1 AND contest = $2\n" +
+            "AND start_date < $3 AND end_date > $3;", [group_id, contest_id, timestamp]);
+        await client.query("COMMIT;");
+    }
+    catch (e) {
+        console.error(e);
+        await client.query("ROLLBACK;");
+        throw(e);
+    }
+    finally {
+        client.release();
+    }
+    if (results !== undefined)
+        return results.rows;
+    else
+        return {};
 };
--- api/router.js
+++ api/router.js
@@ -1,17 +1,23 @@
 const database = require("./database");
-const jsonValidator = require("./jsonValidator")()
+const jsonValidator = require("./jsonValidator")();
+const auth = require("./authorization");
 
 module.exports = function (app) {
 
     app.get("/api/user", async function (req, res) {
         try {
             const id = req.user.id;
-            console.log("GET /api/user for user ID", id);
-            const results = await database.getUser(id);
-            const groups = await database.getUserGroups(id);
-            results.groups = groups;
-            console.log(results);
-            res.send({success: true, data: results});
+            if (id !== undefined) {
+                console.log("GET /api/user for user ID", id);
+                const results = await database.getUser(id);
+                const groups = await database.getUserGroups(id);
+                results.groups = groups;
+                console.log(results);
+                res.send({success: true, data: results});
+            } else {
+                res.status(401);
+                res.send({success: false, message: "User not found"});
+            }
         } catch (e) {
             res.status(500);
             res.send({success: false, message: "Unknown error"});
@@ -44,10 +50,88 @@ module.exports = function (app) {
     app.get("/api/user/groups", async function (req, res) {
         try {
             const id = req.user.id;
-            console.log("GET /api/user for user ID", id);
-            const groups = await database.getUserGroups(id);
-            console.log(groups);
-            res.send({success: true, data: groups});
+            if (id !== undefined) {
+                const id = req.user.id;
+                console.log("GET /api/user for user ID", id);
+                const groups = await database.getUserGroups(id);
+                res.send({success: true, data: groups});
+            } else {
+                res.status(400);
+                res.send({success: false, message: "Invalid data"});
+            }
+        } catch (e) {
+            res.status(500);
+            res.send({success: false, message: "Unknown error"});
+        }
+    });
+
+    app.get("/api/group/:group_id/:contest_id/activities/all", async function (req, res) {
+        try {
+            const id = req.user.id;
+            if (id !== undefined && await auth.checkGroupAuth("member", req.user, req.params.group_id, req.params.contest_id)) {
+                console.log("GET group activities for group", req.params.group_id, req.params.contest_id);
+                const groups = await database.getGroupActivities(req.params.group_id, req.params.contest_id);
+                res.send({success: true, data: groups});
+            } else {
+                console.debug("Unauthorized");
+                res.status(401);
+                res.send({success: false, message: "User not in group"});
+            }
+        } catch (e) {
+            res.status(500);
+            res.send({success: false, message: "Unknown error"});
+        }
+    });
+
+    app.get("/api/group/:group_id/:contest_id/activities/me", async function (req, res) {
+        try {
+            const id = req.user.id;
+            if (id !== undefined && await auth.checkGroupAuth("member", req.user, req.params.group_id, req.params.contest_id)) {
+                console.log("GET group activities for group", req.params.group_id, req.params.contest_id);
+                const groups = await database.getMyGroupActivities(req.params.group_id, req.params.contest_id, id);
+                res.send({success: true, data: groups});
+            } else {
+                console.debug("Unauthorized");
+                res.status(401);
+                res.send({success: false, message: "User not in group"});
+            }
+        } catch (e) {
+            res.status(500);
+            res.send({success: false, message: "Unknown error"});
+        }
+    });
+
+    app.get("/api/group/:group_id/:contest_id/leaderboard", async function (req, res) {
+        try {
+            const id = req.user.id;
+            if (id !== undefined && await auth.checkGroupAuth("member", req.user, req.params.group_id, req.params.contest_id)) {
+                console.log("GET leader board for group", req.params.group_id, req.params.contest_id);
+                const leaderboard = await database.getGroupLeaderBoard(req.params.group_id, req.params.contest_id);
+                res.send({success: true, data: leaderboard});
+            } else {
+                console.debug("Unauthorized");
+                res.status(401);
+                res.send({success: false, message: "User not in group"});
+            }
+        } catch (e) {
+            res.status(500);
+            res.send({success: false, message: "Unknown error"});
+        }
+    });
+
+    app.get("/api/group/:group_id/:contest_id/challenges", async function (req, res) {
+        try {
+            const id = req.user.id;
+            if (id !== undefined && await auth.checkGroupAuth("member", req.user, req.params.group_id, req.params.contest_id)) {
+                const d = new Date();
+                console.log("GET leader board for group", req.params.group_id, req.params.contest_id);
+                const leaderboard = await database.getCurrentContests(req.params.group_id, req.params.contest_id, d.toISOString());
+                res.send({success: true, data: leaderboard});
+            } else {
+                console.debug("Unauthorized");
+                res.status(401);
+                res.send({success: false, message: "User not in group"});
+            }
         } catch (e) {
             res.status(500);
             res.send({success: false, message: "Unknown error"});
