b7a54f63ff8e8664ff2eb90514fbb135d531cf7d
= <leoncai197@gmail.com>
2020-09-04 21:04:17 -0500

    added dropdown to switch between contests

--- client/src/Components/Navigation.js
+++ client/src/Components/Navigation.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react'
-import { Nav, Navbar } from 'react-bootstrap'
+import { Nav, Navbar, NavDropdown } from 'react-bootstrap'
 import Logo from '../Icons/app-logo.svg'
 import '../Styles/Navigation.css'
 
 class Navigation extends Component {
+
+  state = {
+    contest: 'Select Contest'
+  }
+
     render() {
         return (
             <>
@@ -14,6 +19,10 @@ class Navigation extends Component {
                 </Navbar.Brand>
                 <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                 <Navbar.Collapse id="responsive-navbar-nav">
+                  <NavDropdown title={this.state.contest} className="nav-item">
+                    <NavDropdown.Item onClick={(e) => {this.setState({ contest: 'Texas Running Club Fall 2020 Contest'})}}>Texas Running Club Fall 2020 Contest</NavDropdown.Item>
+                    <NavDropdown.Item onClick={(e) => {this.setState({ contest: 'Texas Triathlon Club Fall 2020 Contest'})}}>Texas Triathlon Club Fall 2020 Contest</NavDropdown.Item>
+                  </NavDropdown>
                   <Nav className="ml-auto">
                     <Nav.Link className="nav-item" href="/leaderboard">Leaderboard</Nav.Link>
                     <Nav.Link className="nav-item" href="/activities">Activities</Nav.Link>
