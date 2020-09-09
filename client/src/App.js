a7ca548633202a41288e03aab147a7bcb80ecded
Will Easterby <william.easterby@gmail.com>
2020-09-09 18:51:27 -0500

    And Join modal to ContestOverview

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,7 @@ import Welcome from './Pages/Welcome'
 import Leaderboard from './Pages/Leaderboard'
 import Activities from './Pages/Activities'
 import Challenges from './Pages/Challenges'
-import Contest from "./Pages/Contest"
+import ContestOverview from "./Components/ContestOverview"
 
 class App extends Component {
 
@@ -13,7 +13,7 @@ class App extends Component {
     return (
       <BrowserRouter>
         <Route exact path={'/'} component={Welcome} />
-        <Route path={'/contest/:group/:contest/'} component={Contest} />
+        <Route path={'/contest/:group/:contest/'} component={ContestOverview} />
         <Route path={'/contest/:group/:contest/leaderboard'} component={Leaderboard} />
         <Route path={'/contest/:group/:contest/activities'} component={Activities} />
         <Route path={'/contest/:group/:contest/challenges'} component={Challenges} />
