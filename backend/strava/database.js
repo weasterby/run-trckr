fed2e1a1771f260a90dafe01fa9975966dd6217c
Will Easterby <william.easterby@gmail.com>
2020-09-10 17:34:55 -0500

    Update database tables

--- backend/strava/database.js
+++ backend/strava/database.js
@@ -1,15 +1,15 @@
 const clientPool = require('../db/clientPool');
 
-module.exports.saveAccessToken = async function(oauth) {
+module.exports.saveAccessToken = async function(oauth, scope) {
     const foregroundClient = await clientPool.getNewClient(false);
     const client = foregroundClient.client;
     await client.query("BEGIN;");
     try {
-        await client.query("INSERT INTO users(id, access_token, refresh_token, token_type, expires_at, updated, strava_connected)\n" +
-            "VALUES ($1, $2, $3, $4, $5, CURRENT_TIMESTAMP, true) ON CONFLICT (id) DO UPDATE \n" +
-            "SET access_token = $2, refresh_token = $3, token_type = $4, expires_at = $5,\n" +
+        await client.query("INSERT INTO users(id, access_token, refresh_token, token_type, expires_at, scope, updated, strava_connected)\n" +
+            "VALUES ($1, $2, $3, $4, $5, $6, CURRENT_TIMESTAMP, true) ON CONFLICT (id) DO UPDATE \n" +
+            "SET access_token = $2, refresh_token = $3, token_type = $4, expires_at = $5, scope = $6,\n" +
             "    updated = CURRENT_TIMESTAMP, strava_connected = true;",
-            [oauth.athlete.id, oauth.access_token, oauth.refresh_token, oauth.token_type, new Date(oauth.expires_at * 1000).toISOString()]);
+            [oauth.athlete.id, oauth.access_token, oauth.refresh_token, oauth.token_type, new Date(oauth.expires_at * 1000).toISOString(), scope]);
         await client.query("COMMIT;");
     }
     catch (e){
